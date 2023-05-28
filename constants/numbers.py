0cacd593cab171aa197809eda83a0a5b71095e2e
Sim0nV <78996965+Sim0nV@users.noreply.github.com>
2023-05-28 04:12:16 -0700

    Set win by timeout button timer to 2 minutes

--- constants/numbers.py
+++ constants/numbers.py
@@ -1,4 +1,4 @@
 # Numbers Constants
 WINS_REQUIRED_TO_END = 3  # Round wins required for player to win match
 MATCH_INTERACTION_TIMEOUT = 172800  # Time until match ends by timeout
-TIME_UNTIL_TIMEOUT_BUTTON = 60  # Time until win by timeout button shows
+TIME_UNTIL_TIMEOUT_BUTTON = 120  # Time until win by timeout button shows
